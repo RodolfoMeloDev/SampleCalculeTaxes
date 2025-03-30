7c1265b95a4df1c54aeff6cb0373b539c56e8789
Rodolfo Melo <102770851+RodolfoMeloDev@users.noreply.github.com>
2025-03-30 18:51:39 -0300

    Implementado utilização via Serilog (#9)

--- CalculateTaxes.Services/Services/CalculateTaxesService.cs
+++ CalculateTaxes.Services/Services/CalculateTaxesService.cs
@@ -1,21 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CalculateTaxes.Domain.Interfaces.Services;
+using Microsoft.Extensions.Logging;
 
 namespace CalculateTaxes.Services.Services
 {
-    public class CalculateTaxesService(IFeatureFlagService featureFlagService) : ICalculateTaxesService
+    public class CalculateTaxesService(IFeatureFlagService featureFlagService, ILogger<CalculateTaxesService> logger) : ICalculateTaxesService
     {
         private readonly IFeatureFlagService _featureFlagService = featureFlagService;
+        private readonly ILogger<CalculateTaxesService> _logger = logger;
         public async Task<decimal> ReturnValueTax(decimal value)
         {
             var result = await _featureFlagService.GetByNameFeatureFlag("ReformaTributaria");
 
             if (result != null && result.Active)
+            { 
+                _logger.LogInformation("Utilizando Regra de Cálculo da Reforma Tributária");
                 return value * 0.2m;
+            }
 
+            _logger.LogInformation("Utilizando Regra de Cálculo Convêncional");
             return value * 0.3m;
         }
     }
